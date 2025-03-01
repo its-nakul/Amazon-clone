e5eacf9a998cf36ca41184155d19bce5468409e0
its-nakul <nakulsharmahapur@gmail.com>
2025-03-01 15:39:21 +0530

    Adding header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjfozcjOnTyAwF7vLJBRXIy6WoQhw-Si0",
+  authDomain: "challenge-4ae85.firebaseapp.com",
+  projectId: "challenge-4ae85",
+  storageBucket: "challenge-4ae85.firebasestorage.app",
+  messagingSenderId: "420206353239",
+  appId: "1:420206353239:web:0db5d2254621c0d77ffc12",
+  measurementId: "G-28HHWCZKNT",
+};
